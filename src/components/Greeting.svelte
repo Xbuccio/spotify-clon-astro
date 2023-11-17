97e6518cfb1c900c7379896f571e722f760a23be
Xbuccio <franbuccio@gmail.com>
2023-11-17 13:42:54 -0300

    actualizacion
    14:00

---
+++ src/components/Greeting.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  const currenTime = new Date()
+  const currenHour = currenTime.getHours()
+
+  let greeting = ""
+
+  if (currenHour > 12) {
+    greeting = "Buenos dias"
+  } else if (currenHour < 18) {
+    greeting = "Buenas tarde"
+  } else {
+    greeting = "Buenas noches"
+  }
+</script>
+
+<h1 class="text-3xl font-bold">{greeting}</h1>

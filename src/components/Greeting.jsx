9a9c378d36d9fe27befd330c8c98066d989d9408
Xbuccio <franbuccio@gmail.com>
2023-12-19 13:13:02 -0300

    borre svelte

---
+++ src/components/Greeting.jsx
@@ -0,0 +1,20 @@
+const Greeting = () => {
+  const currentTime = new Date();
+  const currentHour = currentTime.getHours();
+
+  let greeting = "";
+
+  if (currentHour < 12) {
+    greeting = "Buenos días";
+  } else if (currentHour < 18) {
+    greeting = "Buenas tardes";
+  } else {
+    greeting = "Buenas noches";
+  }
+
+  return (
+    <h1 className="text-3xl font-bold">{greeting}</h1>
+  );
+};
+
+export default Greeting;
